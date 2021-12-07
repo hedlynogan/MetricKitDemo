645a6e32535554307f45cef8a9376d0a4c0031a9
Ned Hogan <nedhogan@me.com>
2021-12-06 20:12:14 -0800

    Initial Commit

---
+++ MetricKitPrototype/MetricKitPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricKitPrototypeApp.swift
+//  MetricKitPrototype
+//
+//  Created by Ned Hogan on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MetricKitPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
